05325e24299896a65ba233d5d96f147ff505f8ca
Drew Honson <dhonson@lncrna.caltech.edu>
2023-08-28 15:12:57 -0700

    nojekyll

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'quipt'
+project = 'equipt'
 copyright = '2023, Drew Honson'
 author = 'Drew Honson'
 
@@ -31,6 +31,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+   "nbsphinx",	
 ]
 
 # Add any paths that contain templates here, relative to this directory.
