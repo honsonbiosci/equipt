f6c20a430cfb0c2eb559a6e07e5680bc56163a0f
Drew Honson <dhonson@lncrna.caltech.edu>
2023-09-06 09:01:04 -0700

    Reran

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,6 +52,11 @@ html_theme = 'sphinx_rtd_theme'
 html_favicon = 'images/favicon.ico'
 html_logo = 'images/logo.svg'
 
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
